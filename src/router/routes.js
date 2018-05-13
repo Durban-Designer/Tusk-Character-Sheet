7e4454c62bc767710dd2283837bc6b098f188694
Durban-Designer <royce.birnbaum@gmail.com>
2018-05-13 15:46:35 -0700

    fixed chars

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 
 export default [
   { path: '/', component: () => import('pages/Home') },
+  { path: '/Characters', component: () => import('pages/Characters') },
   { // Always leave this as last one
     path: '*',
     component: () => import('pages/404')
