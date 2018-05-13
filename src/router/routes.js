04eb5f3c72931317cec2bf92d8fdfca52b005e6f
Durban-Designer <royce.birnbaum@gmail.com>
2018-05-13 14:12:46 -0700

    changes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,6 @@
 
 export default [
-  {
-    path: '/',
-    component: () => import('layouts/default'),
-    children: [
-      { path: '', component: () => import('pages/index') },
-      { path: 'characters', component: () => import('pages/characters') }
-    ]
-  },
+  { path: '/',  component: () => import('pages/Home') },
 
   { // Always leave this as last one
     path: '*',
